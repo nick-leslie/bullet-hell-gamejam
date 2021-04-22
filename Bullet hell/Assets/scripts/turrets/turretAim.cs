fa6118a411baac062fa6d53c9fb24aeea68890c5
guy living <nick.leslie303@gmail.com>
2021-04-21 20:30:48 -0400

    started adding turret added player for test prefab

--- Bullet hell/Assets/scripts/turrets/turretAim.cs
+++ Bullet hell/Assets/scripts/turrets/turretAim.cs
@@ -4,15 +4,42 @@ using UnityEngine;
 
 public class turretAim : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject shotPoint;
+    [SerializeField]
+    private bool facingRight;
+    [SerializeField]
+    private bool GunTrackTargert;
+    private turretBrain brain;
     // Start is called before the first frame update
     void Start()
     {
-        
+        brain = gameObject.GetComponent<turretBrain>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        trackTarget();
+        //enemyDirection();
+    }
+    private void trackTarget()
+    {
+        if (GunTrackTargert)
+        {
+            Vector3 dir = brain.target.transform.position - shotPoint.transform.position;
+            float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+
+            transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+            if (dir.x >= 0 && !facingRight)
+            {
+
+                shotPoint.transform.localScale = new Vector3(shotPoint.transform.localScale.x * -1, shotPoint.transform.localScale.y, shotPoint.transform.localScale.z); // activate looking left
+            }
+            else if (dir.x < 0 && facingRight)
+            {
+                shotPoint.transform.localScale = new Vector3(Mathf.Abs(shotPoint.transform.localScale.x), shotPoint.transform.localScale.y, shotPoint.transform.localScale.z); // or activate look right some other way
+            }
+        }
     }
 }
--- Bullet hell/Assets/scripts/turrets/turretBrain.cs
+++ Bullet hell/Assets/scripts/turrets/turretBrain.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class turretBrain : MonoBehaviour
 {
+    public GameObject target;
+    private Room home;
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(target == null)
+        {
+            AsignTarget();
+        }
+    }
+    public void AsignTarget() 
+    {
+        GameObject[] enemys = home.ThingsInRoom.FindAll((GameObject obj) => obj.tag == "enemy").ToArray();
+        int enemyTarget = Random.Range(0, enemys.Length);
+        target = enemys[enemyTarget];
+    }
+    public void AssignHome(GameObject potentalHome)
+    {
+        home = potentalHome.GetComponent<Room>();
     }
 }
